6e1a7172ef28da0b9b3c41a213dd419226c454b1
Zhi4a <sohuga5656@gmail.com>
2022-12-19 17:15:35 +0300

    add class(Player)

--- C2.5/main.py
+++ C2.5/main.py
@@ -129,3 +129,45 @@ class Board:
 
     def begin(self):
         self.busy = []
+
+
+class Player:
+    def __init__(self, board, enemy):
+        self.board = board
+        self.enemy = enemy
+
+    def move(self):
+        while True:
+            try:
+                target = self.ask()
+                repeat = self.enemy.shot(target)
+                return repeat
+            except BoardException as e:
+                print(e)
+
+
+class AI(Player):
+    def ask(self):
+        d = Dot(randint(0, 5), randint(0, 5))
+        print(f"Ход компьютера: {d.x + 1} {d.y + 1}")
+        return d
+
+
+class User(Player):
+    def ask(self):
+        while True:
+            cords = input("Ваш ход:").split()
+
+            if len(cords) != 2:
+                print("Введите 2 координаты")
+                continue
+
+            x, y = cords
+
+            if not (x.isdigit()) or not (y.isdigit()):
+                print("Введите числа")
+                continue
+
+            x, y = int(x), int(y)
+
+            return Dot(x - 1, y - 1)
